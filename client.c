2b867f9af937740433da339bcff6b4f0af5e5faa
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2016-03-24 17:03:36 +0100

    Ajout des fichiers

---
+++ client.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Besoin de <netinet/in.h> ? --> definitions of structures needed for sockets
---
+++ server.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>

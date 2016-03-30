80b88d3e6aa45ad77e1162507c5cb14863e35d7c
Quentin LEVAVASSEUR <Quentin LEVAVASSEUR>
2016-03-30 21:37:31 +0200

    Commande du finale sans commentaire

--- commands/du.c
+++ commands/du.c
@@ -16,40 +16,56 @@
 	Description : 
 		displays the size of a file/directory and his content
 */
-unsigned int recursiveDu(char *nameFile,unsigned int size,int aflag){
+unsigned int recursiveDu(char *nameFile,int aflag,int bflag,int hflag,int kflag,int mflag){
 	struct stat file_stat;
 	DIR *dirp;
 	struct dirent *dptr;
 	char path[1024] = {0};
-	unsigned int fileSize=0;
+	unsigned int size=0;
 	
 	if(isFolder(nameFile)){
-		if(stat(nameFile,&file_stat)!=0){
-			printf("du : directory %s doesn't exist\n",nameFile);
-			exit(1);
-		}
-		fileSize=file_stat.st_size;
 		if ((dirp=opendir(nameFile))==NULL) printf("du : directory %s doesn't exist\n",nameFile);
 		else{
+			if(stat(nameFile,&file_stat)!=0){
+				printf("du : directory %s doesn't exist\n",nameFile);
+				exit(1);
+			}
+			size+=file_stat.st_size;
+				
 			while(dptr=readdir(dirp)){
 				if (!strcmp(dptr->d_name,".") || !strcmp(dptr->d_name,"..")) continue;
 	
 				if (nameFile[strlen(nameFile)-1]=='/') sprintf(path,"%s%s",nameFile,dptr->d_name);
 				else sprintf(path,"%s/%s",nameFile,dptr->d_name);
-	
-				size+=recursiveDu(path,fileSize,aflag);
-				if(aflag==1) printf("%d	%s\n",fileSize,path);
+				
+				if(aflag==1){
+					if(hflag==1){
+						if(bflag==1) printf("%db	%s\n",size,path);
+						else if(mflag==1) printf("%dM	%s\n",(((size/8)/1024)/1024),path);
+						else if(kflag==1) printf("%dK	%s\n",((size/8)/1024),path);
+						else printf("%do	%s\n",(size/8),path);
+					}
+					else{
+						if(bflag==1) printf("%d	%s\n",size,path);
+						else if(mflag==1) printf("%d	%s\n",(((size/8)/1024)/1024),path);
+						else if(kflag==1) printf("%d	%s\n",((size/8)/1024),path);
+						else printf("%d	%s\n",(size/8),path);
+					}
+				}
+				size+=recursiveDu(path,aflag,bflag,hflag,kflag,mflag);
 			}
 			closedir(dirp);
 		}
 	}
 	else if(isRegularFile(nameFile)){
-		stat(nameFile,&file_stat);
-		fileSize=file_stat.st_size;
-		printf("%d	%s\n",fileSize,nameFile);
+		if(stat(nameFile,&file_stat)!=0){
+			printf("du : file %s doesn't exist\n",nameFile);
+			exit(1);
+		}
+		size+=file_stat.st_size;
 	}
 	else{
-		printf("du : file %s doesn't exist\n",nameFile);
+		printf("du : file or directory %s doesn't exist\n",nameFile);
 		exit(1);
 	}
 	
@@ -68,15 +84,27 @@ unsigned int recursiveDu(char *nameFile,unsigned int size,int aflag){
 int main(int argc, char *argv[]){
 	// Declaration of variables
 	unsigned int size=0;
-	int aflag = 0;			// used to know what if option "-a" is required
+	int aflag = 0, bflag=0, hflag=0, kflag=0, mflag=0;			// used to know what if option "-a" or "-k" is required
 	char *nameFile;
 		
 	// We check if option "-a" is required
 	char c;
-	while ( (c = getopt(argc, argv, "a")) != -1 ){
+	while ( (c = getopt(argc, argv, "abhkm")) != -1 ){
 		switch(c){
 			case 'a':
 				aflag = 1;
+				break;
+			case 'b':
+				bflag = 1;
+				break;
+			case 'h':
+				hflag = 1;
+				break;
+			case 'k':
+				kflag = 1;
+				break;
+			case 'm':
+				mflag = 1;
 				break;
 			case '?':
 				printf("rm : option %s doesn't exist for command rm\n",argv[optind-1]);
@@ -85,9 +113,9 @@ int main(int argc, char *argv[]){
 	}
 		
 	// An error is displayed if there is too few arguments
-	if(argc<2){
+	if(argc<2 || (argc==2 && argv[1][0]=='-')){
 		nameFile=".";
-		size+=recursiveDu(nameFile,size,aflag);
+		size+=recursiveDu(nameFile,aflag,bflag,hflag,kflag,mflag);
 	}
 	// Remove the file passed in parameters
 	else{
@@ -95,11 +123,27 @@ int main(int argc, char *argv[]){
 		// For each element we check if it's a directory which exists
 		for (index = optind; index < argc; index++){
 			nameFile=argv[index];
-			size+=recursiveDu(nameFile,size,aflag);
+			/*if(stat(nameFile,&file_stat)!=0){
+				printf("du : directory %s doesn't exist\n",nameFile);
+				exit(1);
+			}
+			size=file_stat.st_size;*/
+			size+=recursiveDu(nameFile,aflag,bflag,hflag,kflag,mflag);
 		}
 	}
 	
-	printf("%d	%s\n",size,nameFile);
+	if(hflag==1){
+		if(bflag==1) printf("%db	%s\n",size,nameFile);
+		else if(mflag==1) printf("%dM	%s\n",(((size/8)/1024)/1024),nameFile);
+		else if(kflag==1) printf("%dK	%s\n",((size/8)/1024),nameFile);
+		else printf("%do	%s\n",(size/8),nameFile);
+	}
+	else{
+		if(bflag==1) printf("%d	%s\n",size,nameFile);
+		else if(mflag==1) printf("%d	%s\n",(((size/8)/1024)/1024),nameFile);
+		else if(kflag==1) printf("%d	%s\n",((size/8)/1024),nameFile);
+		else printf("%d	%s\n",(size/8),nameFile);
+	}
 
 	return 0;
 }

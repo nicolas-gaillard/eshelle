87d7ebbe1668d01cf837dc5bdd7782bac4693beb
ironcreative <q.levavasseur@live.fr>
2016-04-02 22:21:41 +0000

    Commandes ls/du/rm finales et commentées !

--- commands/du.c
+++ commands/du.c
@@ -10,6 +10,35 @@
 
 /*
 	Function name : 
+		displayFileSize 
+	Arguments :
+		char *nameFile (pathname of the file we want to know the size)
+		int ?flag (flags which represent if options are required or not)
+	Description : 
+		displays the size of a file/directory passed in parameter
+*/
+void displayFileSize(char *nameFile,unsigned int size,int hflag,int bflag,int kflag,int mflag){
+	// If option "-h" is required we display the file size its unit
+	if(hflag==1){
+		// If option "-b" is required we display the file size in Bits
+		if(bflag==1) printf("%dB	%s\n",(size*8),nameFile);
+		// If option "-k" is required we display the file size in KiloBytes
+		else if(kflag==1) printf("%dK	%s\n",(size/1024),nameFile);
+		// If option "-m" is required we display the file size in MegaBytes
+		else if(mflag==1) printf("%dM	%s\n",((size/1024)/1024),nameFile);
+		// Else we display the file size in Bytes
+		else printf("%dO	%s\n",size,nameFile);
+	}
+	else{
+		if(bflag==1) printf("%d	%s\n",(size*8),nameFile);
+		else if(kflag==1) printf("%d	%s\n",((size/8)/1024),nameFile);
+		else if(mflag==1) printf("%d	%s\n",(((size/8)/1024)/1024),nameFile);
+		else printf("%d	%s\n",size,nameFile);
+	}
+}
+
+/*
+	Function name : 
 		recursiveDu 
 	Arguments :
 		char *nameFile (pathname of the file we want to know the size)
@@ -17,12 +46,14 @@
 		displays the size of a file/directory and his content
 */
 unsigned int recursiveDu(char *nameFile,int aflag,int bflag,int hflag,int kflag,int mflag){
-	struct stat file_stat;
-	DIR *dirp;
-	struct dirent *dptr;
-	char path[1024] = {0};
-	unsigned int size=0;
+	// Declaration of variables	
+	DIR *dirp;					// the directory we want to know the size
+	struct dirent *dptr;		// used to know the information of the content directory
+	struct stat file_stat;		// used to know the information of the content file/directory
+	char path[1024] = {0};		// a tab which contains the path of a file containing in the directory
+	unsigned int size=0;		// the size of the file/directory
 	
+	// If it's a directory we collect the size of its content files/directories recursively
 	if(isFolder(nameFile)){
 		if ((dirp=opendir(nameFile))==NULL) printf("du : directory %s doesn't exist\n",nameFile);
 		else{
@@ -32,31 +63,24 @@ unsigned int recursiveDu(char *nameFile,int aflag,int bflag,int hflag,int kflag,
 			}
 			size+=file_stat.st_size;
 				
-			while(dptr=readdir(dirp)){
+			while((dptr=readdir(dirp))){
+				// We ignore the "." and ".." directories
 				if (!strcmp(dptr->d_name,".") || !strcmp(dptr->d_name,"..")) continue;
-	
+				// We make the path of the content file/directory
 				if (nameFile[strlen(nameFile)-1]=='/') sprintf(path,"%s%s",nameFile,dptr->d_name);
 				else sprintf(path,"%s/%s",nameFile,dptr->d_name);
-				
+				// If option "-a" is required we display the size of the content files/directories
 				if(aflag==1){
-					if(hflag==1){
-						if(bflag==1) printf("%db	%s\n",size,path);
-						else if(mflag==1) printf("%dM	%s\n",(((size/8)/1024)/1024),path);
-						else if(kflag==1) printf("%dK	%s\n",((size/8)/1024),path);
-						else printf("%do	%s\n",(size/8),path);
-					}
-					else{
-						if(bflag==1) printf("%d	%s\n",size,path);
-						else if(mflag==1) printf("%d	%s\n",(((size/8)/1024)/1024),path);
-						else if(kflag==1) printf("%d	%s\n",((size/8)/1024),path);
-						else printf("%d	%s\n",(size/8),path);
-					}
+					displayFileSize(path,size,hflag,bflag,kflag,mflag);
 				}
+				// We get the size of the content file/directory to add it to the main file size
 				size+=recursiveDu(path,aflag,bflag,hflag,kflag,mflag);
 			}
+			// Directory closure
 			closedir(dirp);
 		}
 	}
+	// Else if it's just a file we get its size
 	else if(isRegularFile(nameFile)){
 		if(stat(nameFile,&file_stat)!=0){
 			printf("du : file %s doesn't exist\n",nameFile);
@@ -64,11 +88,13 @@ unsigned int recursiveDu(char *nameFile,int aflag,int bflag,int hflag,int kflag,
 		}
 		size+=file_stat.st_size;
 	}
+	// Or an error will be displayed
 	else{
 		printf("du : file or directory %s doesn't exist\n",nameFile);
 		exit(1);
 	}
 	
+	// Finally we return the file size
 	return size;
 }
 
@@ -83,11 +109,11 @@ unsigned int recursiveDu(char *nameFile,int aflag,int bflag,int hflag,int kflag,
 */
 int main(int argc, char *argv[]){
 	// Declaration of variables
-	unsigned int size=0;
-	int aflag = 0, bflag=0, hflag=0, kflag=0, mflag=0;			// used to know what if option "-a" or "-k" is required
-	char *nameFile;
+	unsigned int size=0;								// the size of the file/directory
+	int aflag = 0, bflag=0, hflag=0, kflag=0, mflag=0;	// used to know what options are required
+	char *nameFile;										// the of the file/directory we want to know the size
 		
-	// We check if option "-a" is required
+	// We check if option "-a", "-b", "-h", "-k" or "-m" is required
 	char c;
 	while ( (c = getopt(argc, argv, "abhkm")) != -1 ){
 		switch(c){
@@ -111,39 +137,25 @@ int main(int argc, char *argv[]){
 				return -1;
 		}
 	}
-		
-	// An error is displayed if there is too few arguments
-	if(argc<2 || (argc==2 && argv[1][0]=='-')){
+	
+	int index = optind;
+	// If there is no argument we take automatically the current directory
+	if(argv[index]==NULL){
 		nameFile=".";
+		// We get the size of the current directory
 		size+=recursiveDu(nameFile,aflag,bflag,hflag,kflag,mflag);
 	}
-	// Remove the file passed in parameters
+	// Else for each arguments we get and and display its size
 	else{
-		int index;
-		// For each element we check if it's a directory which exists
 		for (index = optind; index < argc; index++){
 			nameFile=argv[index];
-			/*if(stat(nameFile,&file_stat)!=0){
-				printf("du : directory %s doesn't exist\n",nameFile);
-				exit(1);
-			}
-			size=file_stat.st_size;*/
+			// We get the size of the file/directory passed in arguments
 			size+=recursiveDu(nameFile,aflag,bflag,hflag,kflag,mflag);
 		}
 	}
 	
-	if(hflag==1){
-		if(bflag==1) printf("%db	%s\n",size,nameFile);
-		else if(mflag==1) printf("%dM	%s\n",(((size/8)/1024)/1024),nameFile);
-		else if(kflag==1) printf("%dK	%s\n",((size/8)/1024),nameFile);
-		else printf("%do	%s\n",(size/8),nameFile);
-	}
-	else{
-		if(bflag==1) printf("%d	%s\n",size,nameFile);
-		else if(mflag==1) printf("%d	%s\n",(((size/8)/1024)/1024),nameFile);
-		else if(kflag==1) printf("%d	%s\n",((size/8)/1024),nameFile);
-		else printf("%d	%s\n",(size/8),nameFile);
-	}
+	// We display the size of the file/directory
+	displayFileSize(nameFile,size,hflag,bflag,kflag,mflag);
 
 	return 0;
 }
--- commands/ls.c
+++ commands/ls.c
@@ -13,6 +13,72 @@
 
 /*
 	Function name : 
+		advancedDisplay 
+	Arguments : 
+		struct dirent *dptr (the current file/directory)
+		int dflag (flag which represents if option "-d" is required or not)
+	Description : 
+		displays the current content file/directory information on a line
+*/
+void advancedDisplay(struct dirent *dptr,int dflag){
+	// Declaration of variables
+	struct stat st;				// used to know the general information of the content file/directory
+	struct passwd *userInfo;	// used to know the user information of the content file/directory
+    struct group *groupInfo;	// used to know the group information of the content file/directory
+    struct tm *timeInfo;		// used to know the time information of the content file/directory
+    
+    // We collect the information of the content file/directory
+    lstat(dptr->d_name,&st);
+    
+    // We display the file nature
+    if ((st.st_mode & S_IFSOCK)==S_IFSOCK) printf("s");	// socket
+	else if ((st.st_mode & S_IFLNK)==S_IFLNK) printf("l");	// symbolic link
+	else if ((st.st_mode & S_IFREG)==S_IFREG) printf("-");	// regular file
+	else if ((st.st_mode & S_IFBLK)==S_IFBLK) printf("b");	// block device
+	else if ((st.st_mode & S_IFDIR)==S_IFDIR) printf("d");	// directory
+	else if ((st.st_mode & S_IFCHR)==S_IFCHR) printf("c");	// character device
+	else if ((st.st_mode & S_IFIFO)==S_IFIFO) printf("p");	// FIFO
+	
+	// We display the rights according to users
+	printf("%c",(st.st_mode & S_IRUSR)==S_IRUSR ? 'r' : '-');	// owner R
+	printf("%c",(st.st_mode & S_IWUSR)==S_IWUSR ? 'w' : '-');	// owner W
+	printf("%c",(st.st_mode & S_IXUSR)==S_IXUSR ? 'x' : '-');	// owner X
+	printf("%c",(st.st_mode & S_IRGRP)==S_IRGRP ? 'r' : '-');	// group R
+	printf("%c",(st.st_mode & S_IWGRP)==S_IWGRP ? 'w' : '-');	// group W
+	printf("%c",(st.st_mode & S_IXGRP)==S_IXGRP ? 'x' : '-');	// group X
+	printf("%c",(st.st_mode & S_IROTH)==S_IROTH ? 'r' : '-');	// other R
+	printf("%c",(st.st_mode & S_IWOTH)==S_IWOTH ? 'w' : '-');	// other W
+	printf("%c",(st.st_mode & S_IXOTH)==S_IXOTH ? 'x' : '-');	// other X
+	
+	// We display the number of link
+	printf(" %d",st.st_nlink);
+
+	// We display the owner of the file/directory
+	userInfo=getpwuid(st.st_uid);
+	printf(" %s",userInfo->pw_name);
+
+	// We display the owner group of the file/directory
+	groupInfo=getgrgid(st.st_gid);
+	printf(" %s",groupInfo->gr_name);
+
+	// We display the file/directory size
+	printf(" %5d",st.st_size);
+
+	// We display the information about time
+	timeInfo=localtime(&st.st_mtime);
+	printf(" %4d-%02d-%02d %02d:%02d",timeInfo->tm_year+1900,timeInfo->tm_mon+1,timeInfo->tm_mday,timeInfo->tm_hour,timeInfo->tm_min);
+
+	// If option "-d" is not required and if it's a directory the information will be green
+	if(dflag==0 && isFolder(dptr->d_name)){
+		printf("\033[32m");
+		printf(" %s \n",dptr->d_name);
+    	printf("\033[0m");
+	}
+    else printf(" %s \n",dptr->d_name);
+}
+
+/*
+	Function name : 
 		main 
 	Arguments : 
 		int argc (number of passed arguments)
@@ -21,100 +87,93 @@
 		displays the current repertory's content
 */
 int main(int argc,char *argv[]){      
-	DIR *dirp;
-	    struct dirent *dptr;
-	    struct stat st;
-	    
-	    struct passwd *userInfo;
-	    struct group *groupInfo;
-	    struct tm *timeInfo;
-	    
-	if(argv[1]!=NULL){
-	    if(isOption(argv[1])){
-		if(strcmp(argv[1],"-l")==0 || strcmp(argv[1],"-long")==0){
-		    if(isFolder(argv[2])){
-			if((dirp=opendir(argv[2]))==NULL){
-			    printf("Error ! Le dossier %s n'existe pas ou plus !\n",argv[2]);
-			    return(-1);
-			}
-		    }
-		    else{
-			dirp=opendir("./");
-		    }
-		    while(dptr=readdir(dirp)){
-			//printf("%ld   %ld   %d   %s\n",dptr->d_ino,dptr->d_off,dptr->d_reclen,dptr->d_name);
-			//stat(dptr->d_name,&sts);
-			//printf("(st_uid,st_gid,st_ctime/st_mode) : %d   %d   %s   %o\n",sts.st_uid,sts.st_gid,ctime(&sts.st_ctime),sts.st_mode);
-			//printf("\n");
-			
-			lstat(dptr->d_name,&st);
-			if ((st.st_mode & S_IFSOCK)==S_IFSOCK) printf("s");	// socket
-			else if ((st.st_mode & S_IFLNK)==S_IFLNK) printf("l");	// symbolic link
-			else if ((st.st_mode & S_IFREG)==S_IFREG) printf("-");	// regular file
-			else if ((st.st_mode & S_IFBLK)==S_IFBLK) printf("b");	// block device
-			else if ((st.st_mode & S_IFDIR)==S_IFDIR) printf("d");	// directory
-			else if ((st.st_mode & S_IFCHR)==S_IFCHR) printf("c");	// character device
-			else if ((st.st_mode & S_IFIFO)==S_IFIFO) printf("p");	// FIFO
-			
-			printf("%c",(st.st_mode & S_IRUSR)==S_IRUSR ? 'r' : '-');	// owner R
-			printf("%c",(st.st_mode & S_IWUSR)==S_IWUSR ? 'w' : '-');	// owner W
-			printf("%c",(st.st_mode & S_IXUSR)==S_IXUSR ? 'x' : '-');	// owner X
-			printf("%c",(st.st_mode & S_IRGRP)==S_IRGRP ? 'r' : '-');	// group R
-			printf("%c",(st.st_mode & S_IWGRP)==S_IWGRP ? 'w' : '-');	// group W
-			printf("%c",(st.st_mode & S_IXGRP)==S_IXGRP ? 'x' : '-');	// group X
-			printf("%c",(st.st_mode & S_IROTH)==S_IROTH ? 'r' : '-');	// other R
-			printf("%c",(st.st_mode & S_IWOTH)==S_IWOTH ? 'w' : '-');	// other W
-			printf("%c",(st.st_mode & S_IXOTH)==S_IXOTH ? 'x' : '-');	// other X
-			
-			printf(" %d",st.st_nlink);
-
-			userInfo=getpwuid(st.st_uid);
-			printf(" %s",userInfo->pw_name);
-
-			groupInfo=getgrgid(st.st_gid);
-			printf(" %s",groupInfo->gr_name);
-
-			printf(" %5d",st.st_size);
-
-			timeInfo=localtime(&st.st_mtime);
-			printf(" %4d-%02d-%02d %02d:%02d",timeInfo->tm_year+1900,timeInfo->tm_mon+1,timeInfo->tm_mday,timeInfo->tm_hour,timeInfo->tm_min);
-
-			printf(" %s\n",dptr->d_name);
-			      
-		    }
-		    printf("\n");
+	// Declaration of variables	
+	DIR *dirp;						// the directory we want to know the content
+    struct dirent *dptr;			// used to know the information of the directory
+    int aflag=0, dflag=0, lflag=0;	// used to know what options are required
+    
+    // We check if option "-a", "-d" or "-l" is required
+	char c;
+	while ( (c = getopt(argc, argv, "adl")) != -1 ){
+		switch(c){
+			case 'a':
+				aflag = 1;
+				break;
+			case 'd':
+				dflag = 1;
+				break;
+			case 'l':
+				lflag = 1;
+				break;
+			case '?':
+				printf("ls : option %s doesn't exist for command ls\n",argv[optind-1]);
+				return -1;
 		}
-		else{
-		    printf("Error : L'option %s n'existe pas pour cette commande !\n",argv[1]);
-		    return -1;
-		}
-	    }
-	    else if(isFolder(argv[1])){
-		if((dirp=opendir(argv[1]))==NULL){
-		    printf("Error ! Le dossier %s n'existe pas ou plus !\n",argv[1]);
-		    return(-1);
-		}
-		else{
-		    dirp=opendir("./");
-		}
-		while(dptr=readdir(dirp)){
-		    printf("%s ",dptr->d_name);
-		}
-		printf("\n");
-	    }
-	    else{
-		printf("Error ! L'argument %s n'est pas un dossier !\n",argv[1]);
-		return(-1);
-	    }
 	}
-	else{
-	    dirp=opendir("./");
-	    while(dptr=readdir(dirp)){
-		printf("%s ",dptr->d_name);
+		
+	int index = optind;
+	// If there is no file/directory in parameter we take automatically the current directory
+	if(argv[index]==NULL){
+		if ((dirp=opendir("."))==NULL){
+			printf("ls : directory %s doesn't exist\n",".");
+			return -1;
+		}
+		while((dptr=readdir(dirp))){
+	    	// If option "-a" is not required we ignore the "." and ".." directories
+			if(aflag==0) if (!strcmp(dptr->d_name,".") || !strcmp(dptr->d_name,"..")) continue;
+	    	// If option "-l" is required we display the advanced information of the content file/directory
+	    	if(lflag==1){
+	    		advancedDisplay(dptr,dflag);
+	    	}
+	    	else{
+	    		// If option "-d" is not required and if it's a directory the information will be green
+	    		if(dflag==0 && isFolder(dptr->d_name)){
+		    		printf("\033[32m");
+					printf("%s ",dptr->d_name);
+			    	printf("\033[0m");
+		    	}
+			    else printf("%s ",dptr->d_name);
+	    	}
 	    }
 	    printf("\n");
+	    closedir(dirp);
 	}
-	closedir(dirp);
+	// For each arguments we display his information
+	else{
+		for (index = optind; index < argc; index++){
+			// If it's a folder we 
+			if(isFolder(argv[index])){
+				if ((dirp=opendir(argv[index]))==NULL){
+					printf("ls : directory %s doesn't exist\n",argv[index]);
+				}
+				else{
+					while((dptr=readdir(dirp))){
+				    	// We ignore the "." and ".." directories
+						if(aflag==0) if (!strcmp(dptr->d_name,".") || !strcmp(dptr->d_name,"..")) continue;
+				    	// If option "-l" is required we display the advanced information of the content file/directory
+				    	if(lflag==1){
+				    		advancedDisplay(dptr,dflag);
+				    	}
+				    	else{
+				    		// If option "-d" is not required and if it's a directory the information will be green
+				    		if(dflag==0 && isFolder(dptr->d_name)){
+					    		printf("\033[32m");
+								printf("%s ",dptr->d_name);
+						    	printf("\033[0m");
+					    	}
+						    else printf("%s ",dptr->d_name);
+				    	}
+				    }
+				    printf("\n");
+				    closedir(dirp);
+				}
+			}
+			// Else an error is displayed
+			else{
+				printf("ls : directory %s doesn't exist\n",argv[index]);
+			}
+		}
+	}
       
 	return 0;
 }

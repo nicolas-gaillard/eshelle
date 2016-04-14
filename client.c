b655bf4d9c3e185390f267e251d1197ef0285e1d
Valentin <valentin.bouchevreau@etu.univ-nantes.fr>
2016-04-14 18:02:12 +0200

    modif global des sockets

--- client.c
+++ client.c
@@ -1,76 +1,55 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// Note : we have to launch server before client.
-
-// Besoin de <netinet/in.h> ? --> definitions of structures needed for sockets
-
-// recv pour envoyer des messages au serveur
-
+#define PORT 23
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
-#define IP "127.0.0.1"
-#define PORT 2000
+#define closesocket(s) close(s)
 
 typedef int SOCKET;
-typedef struct sockaddr_in sockaddr_in;
-typedef struct sockaddr sockaddr;
-
-void clean(const char *buffer, FILE *fp)
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+ 
+int main(void)
 {
-    char *p = strchr(buffer,'\n');
-    if (p != NULL)
-        *p = 0;
+ 
+    SOCKET sock;
+    SOCKADDR_IN sin;
+    char buffer[32] = "";
+ 
+    /* Création de la socket */
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* Configuration de la connexion */
+    sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(PORT);
+
+    /* Si l'on a réussi à se connecter */
+    if(connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR)
+    {
+        printf("Connection à %s sur le port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
+        
+        /* Si l'on reçoit des informations : on les affiche à l'écran */
+        if(recv(sock, buffer, 32, 0) != SOCKET_ERROR)
+            printf("Recu : %s\n", buffer);
+    }
+    /* sinon, on affiche "Impossible de se connecter" */
     else
     {
-        int c;
-        while ((c = fgetc(fp)) != '\n' && c != EOF);
+        printf("Impossible de se connecter\n");
     }
-}
 
-int main(int argc, char const *argv[]) {
-
-  SOCKET sockClient;
-  sockaddr_in sockClientIn;
-  char buffer[100]="";
-
-  if ((sockClient = socket(AF_INET, SOCK_STREAM, 0)) != INVALID_SOCKET){
-    sockClientIn.sin_addr.s_addr = inet_addr(IP);
-    sockClientIn.sin_family = AF_INET;
-    sockClientIn.sin_port = htons(PORT);
-  }
-  else{
-    perror("Error while creating the socket");
-    // OR :
-    // printf ("ERROR %d : %s", errno, strerror(errno));
-    exit(0);
-  }
-
-  if ((connect(sockClient, (sockaddr*)&sockClientIn, sizeof(sockClientIn)) != SOCKET_ERROR)){
-    printf("Client is connecting from %s on port : %d \n", inet_ntoa(sockClientIn.sin_addr), htons(sockClientIn.sin_port));
-  }
-  else {
-    perror("Unable to connect");
-    close(sockClient);
-    exit(0);
-  }
-
-  while (strcmp(buffer,"quit") != 0){
-    // #!./shell <-- shell launcher
-    fgets(buffer, sizeof(buffer), stdin);
-    clean(buffer, stdin);
-
-    send(sockClient, buffer, sizeof(buffer), 0);
-  }
-
-  // Attemps to connect to the server :
-  close(sockClient);
-  return 0;
+    /* On ferme la socket */
+    closesocket(sock);
+ 
+    /* On attend que l'utilisateur tape sur une touche, puis on ferme */
+    getchar();
+ 
+    return EXIT_SUCCESS;
 }
--- server.c
+++ server.c
@@ -1,102 +1,84 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define PORT 23
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
-#define PORT 2000
-// We begin at 2000, because port 1 to 1024 are reserved (2000 is a random choice)
-
-// To close a socket, use the function close()
-// WARNING, socket is only working on Linux
-
-// WARNING, how to manage port
+#define closesocket(s) close(s)
 
 typedef int SOCKET;
-typedef struct sockaddr_in sockaddr_in;
-typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+ 
+int main(void)
+{
+ 
+    SOCKET sock;
+    SOCKADDR_IN sin;
+    SOCKET csock;
+    SOCKADDR_IN csin;
+    char buffer[32] = "";
+    socklen_t recsize = sizeof(csin);
+    int sock_err;
 
-int main(int argc, char const *argv[]) {
-  // Serveur socket :
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
-  /*
-  * AF_INET for TCP protocol
-  * SOCK_STREAM for TCP protocol
-  * 0 for TCP protocol
-  */
+    /* Si la socket est valide */
+    if(sock != INVALID_SOCKET)
+    {
+        printf("La socket %d est maintenant ouverte en mode TCP/IP\n", sock);
 
-  SOCKET sock;
-  char buffer[100] = "";
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
-    printf("Error while creating the socket. \n");
-    exit(0);
-  }
-  //signal(SIGINT, fin);
+        /* Configuration */
+        sin.sin_addr.s_addr    = htonl(INADDR_ANY);   /* Adresse IP automatique */
+        sin.sin_family         = AF_INET;             /* Protocole familial (IP) */
+        sin.sin_port           = htons(PORT);         /* Listage du port */
+        sock_err = bind(sock, (SOCKADDR*)&sin, sizeof(sin));
 
-  // Socket's settings :
-  sockaddr_in sockServIn;             // Automatic adress
-  sockServIn.sin_family = AF_INET;    // TCP protocol
-  /*
-  INADDR_ANY = allowed us to work without knowing the IP adress of the machine
-  If we want to specify an adress : inet_addr("127.0.0.1")
-  */
-  sockServIn.sin_addr.s_addr = htonl(INADDR_ANY);
-  sockServIn.sin_port = htons(PORT);
+        /* Si la socket fonctionne */
+        if(sock_err != SOCKET_ERROR)
+        {
+            /* Démarrage du listage (mode server) */
+            sock_err = listen(sock, 5);
+            printf("Listage du port %d...\n", PORT);
 
-  // Client socket :
-  SOCKET clientSock;
-  sockaddr_in clientSockAddr;
-  socklen_t clientSize = sizeof(clientSockAddr);
+            /* Si la socket fonctionne */
+            if(sock_err != SOCKET_ERROR)
+            {
+                printf("Tapez une phrase : \n");
+                fgets(buffer, sizeof buffer, stdin);
+                
+                /* Attente pendant laquelle le client se connecte */
+                printf("Patientez pendant que le client se connecte sur le port %d...\n", PORT);        
 
-  // Linking the socket and the struct :
-  if (bind(sock, (sockaddr*)&sockServIn, sizeof(sockServIn)) != SOCKET_ERROR){
-    printf("Waiting a connection with the client on the port : %d \n", PORT);
-    listen(sock, 5);
-    // We are listenning 5 computers at the same time
-  }
-  else{
-    perror("Error while binding");
-    close(sock);
-    exit(0);
-  }
+                csock = accept(sock, (SOCKADDR*)&csin, &recsize);
+                printf("Un client se connecte avec la socket %d de %s:%d\n", csock, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
 
-  // Connection with a client :
-  if ((clientSock = accept(sock, (sockaddr*)&clientSockAddr, &clientSize)) != SOCKET_ERROR){
-    printf("Client is connecting from %s on port : %d \n", inet_ntoa(clientSockAddr.sin_addr), htons(clientSockAddr.sin_port));
+                sock_err = send(csock, buffer, 32, 0);
 
-    while(1){
-      if (recv(sock, buffer, sizeof(buffer), 0) != SOCKET_ERROR){
-        printf("Command received");
-          // Traitement des commandes
-      }
-      else{
-        perror("Command not reveived");
-        shutdown(clientSock, 2);
-        close(clientSock);
-        close(sock);
-        exit(0);
-      }
+                if(sock_err != SOCKET_ERROR)
+                    printf("Chaine envoyée : %s\n", buffer);
+                else
+                    printf("Erreur de transmission\n");
+
+                /* Il ne faut pas oublier de fermer la connexion (fermée dans les deux sens) */
+                shutdown(csock, 2);
+            }
+        }
+
+        /* Fermeture de la socket */
+        printf("Fermeture de la socket...\n");
+        closesocket(sock);
+        printf("Fermeture du serveur terminee\n");
     }
-  }
-  /*
-  Est ce qu'on remplace par :
-  while((clientSock = accept(sock, (sockaddr*)&clientSockAddr, &clientSize)) != SOCKET_ERROR))
-  ou while() <-- chercher une bonne condition d'arrêt, ex quand le client envoie quit, on change
-  un boolean et qui nous fait sortir de la boucle et qui donc quitte tout le protocole
-  */
-  else {
-    perror("Unable to connect");
-    close(sock);
-    exit(0);
-  }
-
-  shutdown(clientSock, 2);
-  close(clientSock);
-  close(sock);
-  exit(0);
+ 
+    /* On attend que l'utilisateur tape sur une touche, puis on ferme */
+    getchar();
+ 
+    return EXIT_SUCCESS;
 }

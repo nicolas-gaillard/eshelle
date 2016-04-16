298a38f4c26599820657002c57d64d7c42c21267
Valentin <valentin.bouchevreau@etu.univ-nantes.fr>
2016-04-16 16:59:33 +0200

    ajout du prompt en commentaire, il manque le shell.h sur git

--- client.c
+++ client.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+//#include <shell.h>
 
 #define PORT 23
 #define INVALID_SOCKET -1
@@ -18,9 +19,11 @@ typedef struct sockaddr SOCKADDR;
 int main(void)
 {
  
+    char currentDir[BUFFER];
+    char hostName[BUFFER];
     SOCKET sock;
     SOCKADDR_IN sin;
-    char buffer[32] = "";
+    char buffer[256] = "";
     int sock_err;
  
     /* Création de la socket */
@@ -37,15 +40,17 @@ int main(void)
         printf("Connection à %s sur le port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
         
         while(1){
+
+          //prompt(currentDir, hostName);
           // On récupere la chaine de caractere à envoyé depuis le clavier
           printf("Tapez votre commande : \n");
           fgets(buffer, sizeof buffer, stdin);
           
           // On l'envoie sur le socket
-          sock_err = send(sock, buffer, 32, 0);
+          sock_err = send(sock, buffer, 256, 0);
           
           // On recupere le message de bonne reception 
-          if(recv(sock, buffer, 32, 0) != SOCKET_ERROR)
+          if(recv(sock, buffer, 256, 0) != SOCKET_ERROR)
               printf("Recu : %s\n", buffer);
         }
     }

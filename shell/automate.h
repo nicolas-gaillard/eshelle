a7a71eeaa13cb3bc6f9fe51b26ff47c2ee95c833
sasigo <jordan.blouin13@gmail.com>
2016-04-16 19:48:28 +0000

    MàJ branche

--- shell/automate.h
+++
@@ -1,5 +0,0 @@
-int isNumber(char c);
-
-int isLetter(char c);
-
-int automate(char command[]);
--- shell/decoupe.h
+++
@@ -1,5 +0,0 @@
-#define SIZE_CUTTING 20
-
-char **str_split (char *s, const char *ct);
-
-char*** decoupe(char *command, int* size);

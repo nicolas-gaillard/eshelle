b802713571264691f65b97d7d7bd62532f6fafdc
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2016-04-17 13:09:23 +0200

    Tentative de mettre le shell en réseau, ne marche pas

---
+++ shell.h
@@ -0,0 +1,9 @@
+#define BUFFER_KEYBOARDING 256
+#define BUFFER 50
+#define NUMBER_FONCTIONS 18
+
+int background(char** cmd[], int size);
+
+void clear();
+
+void prompt(char *currentDir, char *hostName);
